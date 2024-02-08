fb7d6addb078272b315d23aebce6555ff973094b
onixldlc <onxdl16427@outlook.com>
2024-02-09 03:56:03 +0700

    fix: cleanup files

--- client/client-tester.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-ip = "127.0.0.1"
-port = 8008
-message = "[+]client: Hello Server!"
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-sock.sendto(bytes(message, "utf-8"), (ip, port))
